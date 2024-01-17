2dab3bd2e4fb3e040a15ac31568128997b2a8aa2
200103020 <200103020@stu.sdu.edu.kz>
2024-01-17 18:10:29 +0600

    update js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function convertCurrency() {
-    var amount = document.getElementById('amount').value;
-    var currency = document.getElementById('currency').value;
-    var resultElement = document.getElementById('result');
+    let amount = document.getElementById('amount').value;
+    let currency = document.getElementById('currency').value;
+    let resultElement = document.getElementById('result');
 
     sellUSD = 431;
     buyUSD = 429;
@@ -9,16 +9,16 @@ function convertCurrency() {
     sellEuro = 509;
 
     if (currency === 'usd') {
-        var convertedKZT = (amount * buyUSD).toFixed(2);
-        var convertedEUR = (amount * 0.92).toFixed(2);
+        let convertedKZT = parseInt(amount * buyUSD).toFixed(2);
+        let convertedEUR =parseInt(amount * 0.92).toFixed(2);
         resultElement.innerHTML = convertedKZT + ' KZT (tenge),  ' + convertedEUR + ' EUR (euro)';
     } else if (currency === 'eur') {
-        var convertedKZT = (amount * buyEuro).toFixed(2);
-        var convertedUSD = (amount * 1.09).toFixed(2);
+        let convertedKZT = parseInt(amount * buyEuro).toFixed(2);
+        let convertedUSD = parseInt(amount * 1.09).toFixed(2);
         resultElement.innerHTML = convertedKZT + ' KZT (tenge),  ' + convertedUSD + ' USD (dollar)';
     } else if (currency === 'kzt') {
-        var convertedUSD = (amount / buyUSD).toFixed(2);
-        var convertedEUR = (amount / buyEuro).toFixed(2);
+        let convertedUSD = parseInt(amount / buyUSD).toFixed(2);
+        let convertedEUR = parseInt(amount / buyEuro).toFixed(2);
         resultElement.innerHTML = convertedUSD + ' USD (dollar),  ' + convertedEUR + ' EUR (euro)';
     }
 
